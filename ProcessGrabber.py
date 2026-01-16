6da4a759f217a5ce99ce7e79f15efcec20cec8e9
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2026-01-16 00:01:59 -0500

    Update ProcessGrabber.py

--- ProcessGrabber.py
+++ ProcessGrabber.py
@@ -6,5 +6,5 @@ class ProcessGrabber:
         self.processes = [(proc.ProcessId, proc.Name) for proc in self.operatingSystem.Win32_Process()]
         random.shuffle(self.processes)
         
-    def terminateProcess(procID: int):
+    def terminateProcess(self, procID: int):
         self.wmi.Win32_Process(ProcessId=procID).Terminate()
